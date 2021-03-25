4b286644fdd8509a5eb1c373c18930344ee7450c
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-25 19:35:14 +0100

    cleaner code

--- all-playlists/src/components/oAuths.js
+++ all-playlists/src/components/oAuths.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -7,26 +6,33 @@ import { logout } from "../store/user";
 
 const OAuths = (props) => {
   const currentUser = useSelector((state) => state.user.data);
-	const dispatch = useDispatch();
-	const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-	const handleLogout = () => {
-		dispatch(logout());
+  const handleLogout = () => {
+    dispatch(logout());
     history.push("/");
-	};
-    const data = `token=`+localStorage.getItem("accessToken");
-    const state = btoa(data);
+  };
+  const data = `token=` + localStorage.getItem("accessToken");
+  const state = btoa(data);
   return (
-    <div style={{display: "flex", flexDirection: "column", width: "30%", justifyContent: "center", margin: "0 auto"}}>
-    
-    <h1>Welcome {currentUser.username}!</h1>
-      <a href={`http://localhost:5000/api/users/spotifyLogin?state=`+state}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "30%",
+        justifyContent: "center",
+        margin: "0 auto",
+      }}
+    >
+      <h1>Welcome {currentUser.username}!</h1>
+      <a href={`http://localhost:5000/api/users/spotifyLogin?state=` + state}>
         <button>Sign in with Spotify!</button>
       </a>
-      <a href={`http://localhost:5000/api/users/googleRedirect?state=`+state}>
+      <a href={`http://localhost:5000/api/users/googleRedirect?state=` + state}>
         <button>Sign in with Youtube!</button>
       </a>
-      <a href={`http://localhost:5000/api/users/deezerRedirect?state=`+state}>
+      <a href={`http://localhost:5000/api/users/deezerRedirect?state=` + state}>
         <button>Sign in with Deezer!</button>
       </a>
       <button onClick={handleLogout}>Logout</button>

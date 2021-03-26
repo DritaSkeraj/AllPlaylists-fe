c980ea57d13bc1bae145a93682d7b6630d174c2a
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-26 03:04:49 +0100

    bottom player

--- all-playlists/src/pages/mainPage.jsx
+++ all-playlists/src/pages/mainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Player from '../components/player';
 import SideMenu from '../components/sideMenu';
 
 const MainPage = () => {
     return (
         <div>
             <SideMenu/>
+            <Player/>
         </div>
     );
 };

a89fc3d5f8496b888d761e61d4cb013158d4bf8a
DritaSkeraj <37184676+DritaSkeraj@users.noreply.github.com>
2021-03-30 03:41:04 +0200

    adds the css to artist page

--- all-playlists/src/pages/artistPage.jsx
+++ all-playlists/src/pages/artistPage.jsx
@@ -6,7 +6,10 @@ import { withRouter } from "react-router-dom";
 import axios from "axios";
 import SideMenu from "../components/sideMenu";
 import Player from "../components/player";
+import ChartsSong from "../components/chartsSong";
 import "../styles/mainContent.css";
+import "../styles/artistPage.css";
+import { Row, Col } from "react-bootstrap";
 
 const ArtistPage = (props) => {
   const artistId = props.location.pathname.split("/")[2];
@@ -29,11 +32,11 @@ const ArtistPage = (props) => {
     setTop(top.data);
 
     const playlists = await axios.get(
-        `https://cors-anywhere-ds.herokuapp.com/https://api.deezer.com/artist/${artistId}/playlists`
-      );
-      console.log("playlists for this artist: ", playlists.data);
-      setPlaylists(playlists.data);
-    
+      `https://cors-anywhere-ds.herokuapp.com/https://api.deezer.com/artist/${artistId}/playlists`
+    );
+    console.log("playlists for this artist: ", playlists.data);
+    setPlaylists(playlists.data);
+
     const albums = await axios.get(
       `https://cors-anywhere-ds.herokuapp.com/https://api.deezer.com/artist/${artistId}/albums`
     );
@@ -43,66 +46,66 @@ const ArtistPage = (props) => {
 
   return (
     <div>
-    <SideMenu/>
-    <div className="main-container">
-      <Container>
-        <Header />
-        <div>
-          <img src={artist?.picture_big} />
-          <h1>{artist?.name}</h1>
-        </div>
-        <div>
-          <h3>top tracks</h3>
-          {top?.data?.map((res) => (
-            <>
-              <img src={res.album.cover} />
-              <h6>res.title_short</h6>
-              <p>res.artist.name</p>
-              <p>
-                {Math.floor(res.duration / 60)} :{" "}
-                {res.duration - Math.floor(res.duration / 60) * 60}
-              </p>
-            </>
-          ))}
-        </div>
-        <div>
-          <h3>Top playlists:</h3>
-          {playlists?.data?.slice(0.5).map((p) => (
-            <>
-              <img src={p?.picture_small} />
-              <h6>{p?.title}</h6>
-              <p>{p?.user?.name}</p>
-            </>
-          ))}
-        </div>
-        <div>
-            <h3>Albums</h3>
-            {
-                albums?.data?.slice(0, 6).map(a => (
-                    <>
-                    <img src={a?.cover} />
-                        <h6>{a?.title}</h6>
-                    </>
-                ))
-            }
-        </div>
-      </Container>
+      <SideMenu />
+      <div className="main-container" id="artist-pg">
+        <Container>
+          <Header />
+          <Row>
+            <div className="artist-info">
+              <img src={artist?.picture_big} />
+              <h1>{artist?.name}</h1>
+            </div>
+          </Row>
+          <Row className="mt-5">
+            <Col md={7}>
+              <div className="artist-holder">
+                <h3>top tracks</h3>
+                {top?.data?.map((res, key) => (
+                  <ChartsSong data={res} id={key} img={res?.album?.cover} />
+                ))}
+              </div>
+            </Col>
+            <Col md={5}>
+              <div className="artist-holder playlist-holder">
+                <h3>top playlists</h3>
+                {playlists?.data?.slice(0, 6).map((p) => (
+                  <div className="playlist-item">
+                    <Row>
+                    <Col xs={3}>
+                      <img src={p?.picture_small} />
+                      </Col>
+                      <Col xs={8}>
+                        <Row>
+                      <h6>{p?.title}</h6>
+                      </Row>
+                      <Row>
+                      <p>{p?.user?.name}</p>
+                      </Row>
+                      </Col>
+                    </Row>
+                  </div>
+                ))}
+              </div>
+            </Col>
+          </Row>
+          <div className="artist-holder">
+            <h3>albums</h3>
+            <Row>
+            {albums?.data?.slice(0, 6).map((a) => (
+              <>
+              <Col md={4} className="artist-album">
+                <img src={a?.cover} />
+                <h6>{a?.title}</h6>
+                </Col>
+              </>
+            ))}
+            </Row>
+          </div>
+        </Container>
       </div>
-      <Player/>
+      <Player />
     </div>
   );
 };
-{
-  /*
-const ArtistPage = () => {
-    return (
-        <div>
-            <SideMenu/>
-            <Player/>
-            <ArtistContent/>
-        </div>
-    );
-};*/
-}
 
 export default withRouter(ArtistPage);
